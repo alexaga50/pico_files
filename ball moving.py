503e46bc1f8e248781d48e7402abd6bbbdd00742
Alex_Agaliotis <110268084+alexaga50@users.noreply.github.com>
2022-10-01 03:20:46 +0300

    Update ball moving.py

--- ball moving.py
+++ ball moving.py
@@ -2,7 +2,7 @@ from machine import Pin, SPI
 from sh1106 import SH1106_SPI
 import framebuf
 from utime import sleep
-#import screen
+
 
 
 # pins: 4=res(GP2),6=cs(GP4),7=dc(GP5),14=clk(GP10),15=mosi(GP11),vcc,GND
@@ -25,11 +25,11 @@ while True:
     display.fill(0)
     display.rect(x_Pos, y_Pos,ball_sise,ball_sise, 1)
     
-    x_Pos += x_Vel#κίνηση της μπάλας στον άξονα x
-    y_Pos += y_Vel#κίνηση της μπάλας στον άξονα y
-    if x_Pos > x-ball_sise or x_Pos <1:#από το τέλος του άξονα x αφαιρείται το μέγεθος της μπάλας
+    x_Pos += x_Vel
+    y_Pos += y_Vel
+    if x_Pos > x-ball_sise or x_Pos <1:
         x_Vel *= -1
-    if y_Pos >y-ball_sise or y_Pos <1:#από το τέλος του άξονα y αφαιρείται το μέγεθος της μπάλας
+    if y_Pos >y-ball_sise or y_Pos <1:
         y_Vel *= -1
     display.show()
     #sleep(0.001)
